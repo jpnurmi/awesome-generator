bb02037e21fa83c39e96bbfc4d2b75b89337a0f3
J-P Nurmi <jpnurmi@gmail.com>
2021-09-10 23:04:23 +0200

    Provide categories as template context variables

--- lib/src/generator.dart
+++ lib/src/generator.dart
@@ -8,18 +8,27 @@ class AwesomeGenerator {
   AwesomeGenerator({
     required this.template,
     required this.entries,
-  });
+  }) : categories = entries.map((entry) => entry.category).toSet();
 
   final String template;
   final List<AwesomeEntry> entries;
+  final Set<String> categories;
 
   Future<void> generate(String? path) async {
     final context = Context.create();
     context.variables['entries'] = entries.map((p) => p.toJson());
+    for (final category in categories) {
+      context.variables[category] =
+          entries.where((e) => e.category == category).map((p) => p.toJson());
+    }
 
     final content = File(template).readAsStringSync();
     final parsed = Template.parse(context, Source.fromString(content));
-    _output(path).write(await parsed.render(context));
+    _write(path, await parsed.render(context));
+  }
+
+  void _write(String? path, String content) {
+    _output(path).write(content);
   }
 
   IOSink _output(String? output) {
